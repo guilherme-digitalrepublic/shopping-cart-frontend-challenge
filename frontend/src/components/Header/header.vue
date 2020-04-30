feb94a3d6a9ccaea4ae4853e18719a3f03d0de60
Bruno Reis <brm@Bruno-Reis-Mac.local>
2020-04-30 13:51:04 -0300

    Estruct header and icons header

--- frontend/src/components/Header/header.vue
+++ frontend/src/components/Header/header.vue
@@ -1,8 +1,6 @@
 <template>
   <nav class="shop-header">
-    <img class="shop-header__logo" src="@/assets/shop-logo.svg">
-    <div class="shop-header__cart">
-      
-    </div>
+    <img class="shop-header__logo" src="@/assets/icon/shop-logo.svg">
+    <div class="shop-header__cart" src="@/assets/icon/shop-cart.svg"></div>
   </nav>
 </template>
