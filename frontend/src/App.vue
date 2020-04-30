19f98630e9e9ffa29293d78db81f74ba9836625c
Bruno Reis <brm@Bruno-Reis-Mac.local>
2020-04-30 10:01:02 -0300

    Criação projeto e estruturação da build - ShopCart 1.0

---
+++ frontend/src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import Header from './components/Header/index.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style>
+@import "./styles/_index.scss";
+</style>
+
+<template>
+  <Header></Header>
+</template>
---
+++ frontend/src/components/Header/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <nav class="shop-header">
+    <img class="shop-header__logo" src="@/assets/shop-logo.svg">
+    <div class="shop-header__cart">
+      
+    </div>
+  </nav>
+</template>
