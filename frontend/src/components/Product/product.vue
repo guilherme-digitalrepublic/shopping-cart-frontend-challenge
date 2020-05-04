9b4c45ea488808501d3a5794d850df61dfaba0e6
Bruno Reis <brm@Bruno-Reis-Mac.local>
2020-05-04 16:04:37 -0300

    struct products

---
+++ frontend/src/components/Product/product.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="shop-product">
+    <img class="shop-product__image">
+    <div class="shop-product__name"></div>
+    <div class="shop-product__price"></div>
+  </div>
+</template>
