3ba6ae3dfdb55142512309be89b3a8549bcb8f71
Bruno Reis <brm@Bruno-Reis-Mac.local>
2020-05-05 11:05:52 -0300

    Struct button add

--- frontend/src/components/Product/product.vue
+++ frontend/src/components/Product/product.vue
@@ -3,6 +3,7 @@
     <img class="shop-product__image">
     <div class="shop-product__name"></div>
     <div class="shop-product__price"></div>
+    <button class="shop-product__add"></button>
     <button class="shop-product__delet"></button>
   </div>
 </template>
