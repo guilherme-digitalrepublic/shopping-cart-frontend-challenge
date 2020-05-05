66eab00c739200892c26f9e6554390816e97c2ce
Bruno Reis <brm@Bruno-Reis-Mac.local>
2020-05-05 11:03:40 -0300

    struct products

--- frontend/src/components/Product/product.vue
+++ frontend/src/components/Product/product.vue
@@ -3,5 +3,12 @@
     <img class="shop-product__image">
     <div class="shop-product__name"></div>
     <div class="shop-product__price"></div>
+    <button class="shop-product__delet"></button>
   </div>
 </template>
+
+<script>
+export default {
+  
+}
+</script>
