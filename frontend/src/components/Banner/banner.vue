1d6ba7062a79de7be2352cfe575909a8ae69763f
Bruno Reis <brm@Bruno-Reis-Mac.local>
2020-05-07 12:27:21 -0300

    Struct Banner

---
+++ frontend/src/components/Banner/banner.vue
@@ -0,0 +1,7 @@
+<template>
+  <section class="shop-banner">
+    <div class="shop-banner__container">
+      <h1 class="shop-banner__title"><span class="shop-banner__title-name">ShopCart<span> Uma Nova Experiência</h1>
+    </div>
+  </section>
+</template>
